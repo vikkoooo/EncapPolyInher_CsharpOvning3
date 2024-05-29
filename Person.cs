51624c28c3b26ba9f957ff9c21e7a66e7b359b83
vikkoooo <vikkolundberg@gmail.com>
2024-05-29 10:23:53 +0200

    implement some checks in person

--- Person.cs
+++ Person.cs
@@ -11,18 +11,45 @@ internal class Person
 	public int Age
 	{
 		get { return age; }
-		set { age = value; }
+		set
+		{
+			if (value <= 0)
+			{
+				throw new ArgumentException("Age can not be less than or equal to zero");
+			}
+			else
+			{
+				age = value;
+			}
+		}
 	}
 
 	public string FName
 	{
 		get { return fName; }
-		set { fName = value; }
+		set
+		{
+			if (value.Length < 2 || value.Length > 10)
+			{
+				throw new ArgumentException("First name can not be less than two characters long or more than ten characters long");
+			}
+			else
+			{
+				fName = value;
+			}
+		}
 	}
 
 	public string LName
 	{
 		get { return lName; }
-		set { lName = value; }
+		set { lName = value; } // todo implement check
+	}
+
+
+	public Person(string fName, string lName)
+	{
+		FName = fName;
+		LName = lName;
 	}
 }
