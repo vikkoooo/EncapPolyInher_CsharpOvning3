e5ce8445f6964116d219536fb00783b05a8ef007
vikkoooo <vikkolundberg@gmail.com>
2024-05-30 10:03:58 +0200

    3.1 Encapsulation

--- Person.cs
+++ Person.cs
@@ -1,55 +1,103 @@
-internal class Person
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
 {
-	// Fields
-	private int age;
-	private string fName;
-	private string lName;
-	private int height;
-	private int weight;
-
-	// Getters & setters
-	public int Age
+	internal class Person
 	{
-		get { return age; }
-		set
+		// Fields
+		private int age;
+		private string fName;
+		private string lName;
+		private double height;
+		private double weight;
+
+		// Constructors
+		// todo think about the constructors one more time, is this the way to go?
+		public Person(string fName, string lName)
 		{
-			if (value <= 0)
+			FName = fName;
+			LName = lName;
+		}
+
+		public Person(int age, string fName, string lName, double height, double weight)
+		{
+			FName = fName;
+			LName = lName;
+			Age = age;
+			Height = height;
+			Weight = weight;
+		}
+
+		// Getters & setters
+		public int Age
+		{
+			get { return age; }
+			set
 			{
-				throw new ArgumentException("Age can not be less than or equal to zero");
-			}
-			else
-			{
-				age = value;
+				if (value <= 0)
+				{
+					throw new ArgumentException("Age can not be less than or equal to zero");
+				}
+				else
+				{
+					age = value;
+				}
 			}
 		}
-	}
 
-	public string FName
-	{
-		get { return fName; }
-		set
+		public string FName
 		{
-			if (value.Length < 2 || value.Length > 10)
+			get { return fName; }
+			set
 			{
-				throw new ArgumentException("First name can not be less than two characters long or more than ten characters long");
-			}
-			else
-			{
-				fName = value;
+				if (string.IsNullOrWhiteSpace(value) || value.Length < 2 || value.Length > 10)
+				{
+					throw new ArgumentException("First name can not be less than two characters long or more than ten characters long");
+				}
+				else
+				{
+					fName = value;
+				}
 			}
 		}
-	}
 
-	public string LName
-	{
-		get { return lName; }
-		set { lName = value; } // todo implement check
-	}
+		public string LName
+		{
+			get { return lName; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value) || value.Length < 3 || value.Length > 15)
+				{
+					throw new ArgumentException("Last name can not be less than three characters long or more than fifteen characters long");
+				}
+				else
+				{
+					lName = value;
+				}
+			}
+		}
 
+		public double Height
+		{
+			get { return height; }
+			set { height = value; }
+		}
 
-	public Person(string fName, string lName)
-	{
-		FName = fName;
-		LName = lName;
+		public double Weight
+		{
+			get { return weight; }
+			set { weight = value; }
+		}
+
+		// Util
+		public override string ToString()
+		{
+			return $"Firstname: {FName}, Lastname: {LName}, Age: {Age}, Height: {height} cm, Weight: {weight} kg";
+		}
 	}
 }
+
---
+++ PersonHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+	internal class PersonHandler
+	{
+		public Person CreatePerson(int age, string fName, string lName, double height, double weight)
+		{
+			return new Person(age, fName, lName, height, weight);
+		}
+
+		public void SetAge(Person pers, int age)
+		{
+			pers.Age = age;
+		}
+
+		public void SetFirstname(Person pers, string fName)
+		{
+			pers.FName = fName;
+		}
+
+		public void SetLastname(Person pers, string lName)
+		{
+			pers.LName = lName;
+		}
+
+		public void SetHeight(Person pers, double height)
+		{
+			pers.Height = height;
+		}
+
+		public void SetWeight(Person pers, double weight)
+		{
+			pers.Weight = weight;
+		}
+	}
+}
