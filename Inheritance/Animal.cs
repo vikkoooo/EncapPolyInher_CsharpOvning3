a63acd04ef6d2593c6255adebcb040b72a13b9b9
vikkoooo <vikkolundberg@gmail.com>
2024-06-05 16:32:20 +0200

    fix implementation of props and constructor

--- Inheritance/Animal.cs
+++ Inheritance/Animal.cs
@@ -8,14 +8,20 @@ namespace Inheritance
 {
 	internal abstract class Animal
 	{
-		// Animal propertys
-		public abstract string Name { get; protected set; }
-		public abstract double Weight { get; protected set; }
-		public abstract int Age { get; protected set; }
-		public abstract int CutnessLevel { get; protected set; }
+		// Animal properties
+		public string Name { get; protected set; }
+		public double Weight { get; protected set; }
+		public int Age { get; protected set; }
+		public int CutnessLevel { get; protected set; }
 
 		// Constructor
-		public Animal() { }
+		protected Animal(string name, double weight, int age, int cutnessLevel)
+		{
+			Name = name;
+			Weight = weight;
+			Age = age;
+			CutnessLevel = cutnessLevel;
+		}
 
 		// Animal Functions
 		public abstract string DoSound();
